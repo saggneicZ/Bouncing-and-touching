ff36426261703cb455ecf577118524b278ded8e7
saggneicZ <72294761+saggneicZ@users.noreply.github.com>
2020-10-22 13:35:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box1,box2;
+
+function setup() {
+
+  createCanvas(800,400);
+box1=createSprite(200,200,50,50);
+box1.shapeColor="green";
+box2=createSprite(600,200,50,50);
+box2.shapeColor="green";
+box1.velocityX=2;
+box2.velocityX=-2;
+}
+
+function draw() {
+  background(255,255,255);  
+ // box2.x=World.mouseX;
+  //box2.y=World.mouseY;
+  if(box2.x-box1.x<=box1.width/2+box2.width/2 &&
+    box1.x-box2.x<=box1.width/2+box2.width/2 &&
+    box2.y-box1.y<=box1.height/2+box2.height/2 &&
+    box1.y-box2.y<=box1.height/2+box2.height/2){
+
+//box1.shapeColor="red"; 
+  //  box2.shapeColor="red";
+
+  box1.velocityX=box1.velocityX*(-1);
+  box1.velocityX=box1.velocityX*(-1);
+    }
+
+    else{
+     // box1.shapeColor="green";
+     // box2.shapeColor="green";
+      box1.velocityX=box1.velocityX;
+      box2.velocityX=box2.velocityX;
+    }
+ 
+
+  drawSprites();
+}
